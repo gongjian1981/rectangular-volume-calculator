287eaa65dcb25ba63cce50baa6d682cd01874c2a
Jian Gong <gongjian1981@gmail.com>
2025-02-05 14:59:06 -0500

    volume_calculator: edit main script to validate parameter number

--- volume_calculator.sh
+++ volume_calculator.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ "$#" -ne 3 ]; then
+    echo "You should pass three parameters since you are calculating the volume of a rectangular prism."
+    exit 1
+fi
+
 Height=$1
 Width=$2
 Length=$3
