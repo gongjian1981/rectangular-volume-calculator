28d77adba67ff7a319cc09d89917fa752d1b59e6
Jian Gong <gongjian1981@gmail.com>
2025-02-05 14:38:18 -0500

    test_calculate_volumne_valid_result: delete log info for debugging

--- test_calculate_volumne_valid_result.sh
+++ test_calculate_volumne_valid_result.sh
@@ -7,9 +7,6 @@ ExpectedVolume=24
 
 CalculatedVolume=`sh ./volume_calculator.sh $Height $Width $Length`
 
-echo 'expected result is '$ExpectedVolume
-echo 'calculated result is '$CalculatedVolume
-
 if [[ $CalculatedVolume == $ExpectedVolume ]];
 then 
 	echo 'SUCCESS'
