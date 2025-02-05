980cc4b1ea9d2fcb1b02ac0b377a64dec0568a85
Jian Gong <gongjian1981@gmail.com>
2025-02-05 15:00:13 -0500

    test_calculate_volumne_with_passing_no_parameter: delete log info for debugging

--- test_calculate_volumne_with_passing_no_parameter.sh
+++ test_calculate_volumne_with_passing_no_parameter.sh
@@ -3,9 +3,6 @@
 ExpectedResult='You should pass three parameters since you are calculating the volume of a rectangular prism.'
 ActualResult=`sh ./volume_calculator.sh`
 
-echo 'ExpectedResult='$ExpectedResult
-echo 'ActualResult='$ActualResult
-
 if [[ $ExpectedResult == $ActualResult ]];
 then
 	echo 'SUCCESS'
