4761c985ec0452893dd6d6dba68c9cfce8423845
Jian Gong <gongjian1981@gmail.com>
2025-02-05 14:23:18 -0500

    test_calculate_volumne_valid_result: write code to test the calculate method

--- test_calculate_volumne_valid_result.sh
+++ test_calculate_volumne_valid_result.sh
@@ -1,3 +1,18 @@
-#!/bin/sh
+#!/bin/bash
 
+Height=2
+Width=3
+Length=4
+ExpectedVolume=24
 
+CalculatedVolume = `sh ./calculate $Height $Width $Length`
+
+echo 'expected result is '$ExpectedVolume
+echo 'calculated result is '$CalculatedVolume
+
+if [[ $CalculatedVolume eq $ExpectedVolume ]];
+then 
+	echo 'SUCCESS'
+else
+	echo 'FAILURE'
+fi
